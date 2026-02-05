e18643804c514f5ef717145b531c367b16148ab5
Elias <elias_rrosa@hotmail.com>
2026-02-05 13:04:10 -0300

    code from lesson 0

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { FeedbackProvider } from "@eliasrrosa/react-ui";
 import MainPage from "./components/pages/MainPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="w-full grid grid-cols-1 justify-items-center">
+    <QueryClientProvider client={queryClient}>
+      <div className="w-full grid grid-cols-1 justify-items-center">
         <FeedbackProvider>
           <MainPage />
         </FeedbackProvider>
-    </div>
+      </div>
+    </QueryClientProvider>
   );
 }
 
