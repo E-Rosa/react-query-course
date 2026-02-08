b28753fddabb2123c1b2614f3383e1ceecdeaabf
Elias <elias_rrosa@hotmail.com>
2026-02-08 10:03:46 -0300

    lesson-0

--- src/components/books/PaginatedReadBooks.tsx
+++ src/components/books/PaginatedReadBooks.tsx
@@ -1,38 +1,30 @@
 import { VinminPagination } from "@eliasrrosa/vinmin";
 import Book, { Book as BookT } from "../readBooks/Book";
 import { useNavigate, useSearchParams } from "react-router";
-import { useQuery } from "@tanstack/react-query";
-import { fetchReadBooks } from "../../repo/bookRepo";
-import { GetBooksResponse } from "../../hooks/bookHooks";
 
 function PaginatedReadBooks() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const searchParamsOffset = searchParams.get("offset");
   const offset = searchParamsOffset ? parseInt(searchParamsOffset) : 0;
-  const readBooks = useQuery({
-    queryFn: async () => {
-      const res = await fetchReadBooks({
-        take: 5,
-        offset: 0,
-      });
-
-      return await res.json() as GetBooksResponse;
-    },
-    queryKey: ["getPaginatedBooks"],
-  });
-
+  const readBooks = [{
+    id: "id",
+    author: "Loading...",
+    title: "Loading...",
+    rating: 0
+  }];
+  
   return (
     <>
       <div className="flex flex-col gap-4">
-        {readBooks.data?.books?.map((book, key) => {
+        {readBooks.map((book, key) => {
           return <Book book={book} key={key} />;
         })}
         <VinminPagination
           className="self-center mt-2"
           itemsPerPage={5}
           offset={offset}
-          totalItemsCount={readBooks.data?.totalBooksCount}
+          totalItemsCount={readBooks.length}
           onPageClick={(opts) => {
             searchParams.set("offset", `${opts.offset}`);
             navigate(`?${searchParams.toString()}`);
