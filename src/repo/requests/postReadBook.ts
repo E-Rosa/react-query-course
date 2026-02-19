3bbb8094770e6eac11de4843bbac47375f1c89bb
Elias <elias_rrosa@hotmail.com>
2026-02-19 10:12:41 -0300

    merged: readBooks dir into books dir

--- src/repo/requests/postReadBook.ts
+++ src/repo/requests/postReadBook.ts
@@ -1,4 +1,4 @@
-import { Book } from "../../components/readBooks/Book";
+import { Book } from "../../components/books/Book";
 import { LocalStorage } from "../../storage/localStorage";
 import { MockBody, MockFetch, MockResponse } from "@eliasrrosa/mock-fetch";
 import { BookTags } from "../../storage/data/tags";
