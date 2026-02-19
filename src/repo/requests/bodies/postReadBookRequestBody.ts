65859853002964066567b325c2ddffa5160a96da
Elias <elias_rrosa@hotmail.com>
2026-02-19 10:12:41 -0300

    merged: readBooks dir into books dir

--- src/repo/requests/bodies/postReadBookRequestBody.ts
+++ src/repo/requests/bodies/postReadBookRequestBody.ts
@@ -1,4 +1,4 @@
-import { Book } from "../../../components/readBooks/Book";
+import { Book } from "../../../components/books/Book";
 
 export type PostReadBookRequestBody = {
   book: Omit<Book, "id">;
